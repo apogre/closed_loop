55b3758fcc8440e2a1f8986580e74660d060dfc7
apogre <mail.anishpradhan@gmail.com>
2016-04-05 17:08:24 -0700

    connect.py

--- connect.py
+++ connect.py
@@ -13,12 +13,17 @@ print 'connected'
 while True:
 	count == 0
 	level_count = 1
+	zero_count = 0
 	data = s.recv(1000)
 	if "EmotivAffectiv" in data:
 		print data
 		a_split = data.split(';')
 		new_fr = a_split[8]
+		f = open("test.txt","a") #opens file with name of "test.txt"
+		f.write(new_fr)
+		f.close()
 		if new_fr<0:
+			zero_count = zero_count+1
 			new_fr = 0
 		if count < 233:
 			count = count+1
@@ -34,8 +39,25 @@ while True:
 					new_frustration1 = (old_fr/233)
 				if new_frustration1 < new_frustration:
 					#lower the level
+					 l = open("C:\\Users\\apradha7\\Downloads\\git2016\\closed_loop\\pixelracer\\controllers\\level_val","r+").read()
+					 if l > 1:
+					 	f = open("C:\\Users\\apradha7\\Downloads\\git2016\\closed_loop\\pixelracer\\controllers\\level_val","w")
+					 	f.write(l-1)
+					 	f.close()
 				else:
 					#increase the level
+					lp = open("C:\\Users\\apradha7\\Downloads\\git2016\\closed_loop\\pixelracer\\controllers\\level_val","r+").read()
+					 if lp > 1:
+					 	fp = open("C:\\Users\\apradha7\\Downloads\\git2016\\closed_loop\\pixelracer\\controllers\\level_val","w")
+					 	fp.write(l+1)
+					 	fp.close()
+			else:
+				li = open("C:\\Users\\apradha7\\Downloads\\git2016\\closed_loop\\pixelracer\\controllers\\level_val","r+").read()
+				if li == 1:
+					fi = open("C:\\Users\\apradha7\\Downloads\\git2016\\closed_loop\\pixelracer\\controllers\\level_val","w")
+					fi.write(l+1)
+					fi.close()
 			level_count = level_count+1
 			count = 0
+			zero_count = 0
 		# print '\n'
