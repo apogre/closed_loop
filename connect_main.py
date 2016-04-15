6470b0acef7d384c474d09f53d7efe141a3d3ea2
apogre <mail.anishpradhan@gmail.com>
2016-04-15 11:30:59 -0700

    update level

--- connect_main.py
+++ connect_main.py
@@ -2,24 +2,36 @@ import csv
 from time import sleep
 import sys
 import datetime
+from pixelracer.models import Affectiv, db
+from datetime import datetime
+import socket, sys, time
 
 path = "C:\\Users\\apradha7\\Downloads\\git2016\\closed_loop\\pixelracer\\controllers\\level_val"
+path1 = "C:\\Users\\apradha7\\Downloads\\git2016\\closed_loop\\pixelracer\\controllers\\level_val_updates"
+
 count = 0
 level_count = 1
 hold = 0
 alert_flag = 0
 addon_fr = 0
-with open('export.txt') as infile:
-	reader = csv.reader(infile)
-	for row in reader:
-		# sleep(1)
-		# print row		
-		
-		# zero_count = 0
-		start_time = datetime.datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S")
+
+HOST = 'localhost'
+PORT = 8088
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s_add = (HOST, PORT)
+print sys.stderr, 'connecting'
+s.connect(s_add)
+print 'connected'
+while True:
+	data = s.recv(1000)
+	if "EmotivAffectiv" in data:
+		# print data
+		a_split = data.split(';')
+		start_time = datetime.datetime.now()
 		if count == 0:
 			end_time = start_time + datetime.timedelta(minutes=1)
-		new_fr = float(row[1])
+		new_fr = float(a_split[8])
 		if start_time <= end_time:
 			count = count+1
 			if count>1:
@@ -48,12 +60,18 @@ with open('export.txt') as infile:
 							fp = open(path,"w")
 							fp.write(str(lp+1))
 							fp.close()
+							fp1 = open(path1,"a")
+							fp1.write(str(lp+1)+'\n')
+							fp1.close()
 					else: #Reduce Level and Start Over
 						alert_flag = 0
 						if lp > 1:
 							fp = open(path,"w")
 							fp.write(str(lp-1))
 							fp.close()
+							fp1 = open(path1,"a")
+							fp1.write(str(lp-1)+'\n')
+							fp1.close()
 
 				else: #increase in frustration
 					alert_flag = 1
@@ -67,6 +85,9 @@ with open('export.txt') as infile:
 						fp = open(path,"w")
 						fp.write(str(lp+1))
 						fp.close()
+						fp1 = open(path1,"a")
+						fp1.write(str(lp+1)+'\n')
+						fp1.close()
 			else:
 				new_frustration = old_fr/count
 				f = open(path)
@@ -77,7 +98,10 @@ with open('export.txt') as infile:
 				if li == 1:
 					fi = open(path,"w")
 					fi.write(str(li+1))
-					fi.close()			
+					fi.close()
+					fp1 = open(path1,"a")
+					fp1.write(str(lp+1)+'\n')
+					fp1.close()			
 			level_count = level_count+1
 			count = 0
 			addon_fr = new_fr
